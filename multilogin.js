a5f750f319d0fb570d485cacc2df33d3d39b40eb
Julien <julien@MacBook-Pro-de-Julien.local>
2026-05-27 12:38:43 +0200

    v0.0.14: iframe creee une seule fois, evite le refresh qui interrompt la sequence

--- multilogin.js
+++ multilogin.js
@@ -12,14 +12,19 @@ module.exports.multilogin = function (parent) {
     obj.server_startup = function () {};
 
     // Injecte un onglet "Multi-Login" dans l'UI MeshCentral
+    // IMPORTANT : ne recreer l'iframe que si elle n'existe pas deja,
+    // sinon chaque refresh du device interrompt la sequence en cours
     obj.onDeviceRefreshEnd = function () {
         pluginHandler.registerPluginTab({
             tabTitle: "Multi-Login",
             tabId: "pluginMultilogin"
         });
-        QA('pluginMultilogin',
-            '<iframe src="/pluginadmin.ashx?pin=multilogin&user=1" ' +
-            'style="width:100%;height:760px;border:0"></iframe>');
+        var container = document.getElementById('pluginMultilogin');
+        if (container && !container.querySelector('iframe')) {
+            QA('pluginMultilogin',
+                '<iframe src="/pluginadmin.ashx?pin=multilogin&user=1" ' +
+                'style="width:100%;height:760px;border:0"></iframe>');
+        }
     };
 
     // Sert la vue handlebars quand on appelle /pluginadmin.ashx?pin=multilogin
